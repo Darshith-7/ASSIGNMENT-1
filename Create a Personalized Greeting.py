f8841830f890c0048984fa4215f5e8f4fa3bd3f3
Darshith-7 <r.darshith.rao@gmail.com>
2025-03-22 11:17:24 +0530

    Add files via upload

---
+++ Create a Personalized Greeting.py
@@ -0,0 +1,5 @@
+a = input('Enter your first name: ')
+b = input('Enter your last name: ')
+name = a + " " + b 
+c = "Hello, " + name + "! Welcome to the Python program."
+print(c)
---
+++ Perform Basic Mathematical Operations.py
@@ -0,0 +1,11 @@
+a=int(input('Enter the first number'))
+b=int(input('Enter the second number'))
+addition=a+b
+subtraction=a-b
+multiplication=a*b
+division=a/b
+print()
+print('Addition: ',addition)
+print('Subtraction: ',subtraction)
+print('Multiplication: ',multiplication)
+print('Division: ',division)
